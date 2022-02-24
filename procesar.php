79b73e6ff2265c5f86a60421f4a1fef7c5e7e464
jmaviles1989 <99872785+jmaviles1989@users.noreply.github.com>
2022-02-24 23:43:52 +0100

    Add files via upload

---
+++ procesar.php
@@ -0,0 +1,11 @@
+<?php
+$nombre = $_GET["nombre"]
+$asunto = $_GET["asunto"]
+$mensaje = $_GET["mensaje"]
+
+
+echo "<h2>información php </h2>"
+echo  "EL nombre es :" . $nombre . "<br/>";
+echo  "EL asunto es :" . $asunto . "<br/>";
+echo  "EL mensaje es :" . $mensaje . "<br/>";
+?>
